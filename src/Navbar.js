0345fc326d6a643bb871c08e392420e65533bf8b
Mike <michalkaso@yahoo.com>
2022-06-08 18:54:53 +0300

    airbnb duplicate

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={require('./airbnb-logo.png')} alt="logo" className="nav--logo"/>
+        </nav>
+
+        
+    )
+}
